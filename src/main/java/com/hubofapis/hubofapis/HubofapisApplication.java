687716260a625711be7a63edfd819c6829c66ab1
ig-imanish <manishsailakhurd83@gmail.com>
2024-03-17 18:54:20 +0530

    first commit

---
+++ src/main/java/com/hubofapis/hubofapis/HubofapisApplication.java
@@ -0,0 +1,13 @@
+package com.hubofapis.hubofapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubofapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubofapisApplication.class, args);
+	}
+
+}
